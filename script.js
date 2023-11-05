91185f7ec306b2d04aca8816a10d10815b56b68d
strangerro <149906609+strangerro@users.noreply.github.com>
2023-11-05 22:35:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function guessNumber(){
     } else if (enteredNumber < randomNum) {
         answer = "Число більше вказаного!";
     } else if (isNaN(enteredNumber)) {
-        answer = "Для початку роботи потрібно ввести число"
+        answer = "Для початку роботи потрібно ввести число";
     }
     console.log(answer);
     document.getElementById("text").innerHTML = answer;
@@ -42,59 +42,3 @@ function guessNumber(){
 
 
 
-
-
-
-
-
-
-/*let number = 142;
-let enteredNumber;
-let answer;
-let numberInput = document.getElementById("numberInput");
-let sendButton = document.getElementById("sendButton");
-
- function reloadPage() {
-            setTimeout(reloadPage, 20000)
-            location.reload()
-        }
-
-
-sendButton.addEventListener("click", function(guessNumber) {
-    enteredNumber = parseInt(numberInput.value);
-        console.log(enteredNumber);
-guessNumber();
-
-function guessNumber(){
-    if (enteredNumber === number) {
-        answer = "Вы вгадали число!";
-    } else if (enteredNumber > number) {
-        answer = "Число меньше вказаного!";
-    } else if (enteredNumber < number) {
-        answer = "Число більше вказаного!";
-    }
-    console.log(answer);
-    document.getElementById("text").innerHTML = answer;
-}})
-
-
-
-
-let counter = document.getElementById("clickCount");
-sendButton.addEventListener("click", function() {
-    function clickCounter () {
-        counter++;
-        
-
-    }
-})
-
-
-*/
-
-
-
-/* <input type = "number" id = "numberInput" placeholder = "Введіть число">
-<button id = "sendButton">Підтвердити</button>
-<p id = "text">Для початку роботи введіть число від 1 до 200</p>
-<p> Кількість спроб: <span id = "clickCount"> 0 </span></p> */
